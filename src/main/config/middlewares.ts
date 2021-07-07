da8c5606035febc1de2e3c068cd23dd25cb3e9ae
leonardopliski <leonardopliski@gmail.com>
2021-07-06 23:28:00 -0300

    feat: ensure express content type is json by default

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
+import { contentType } from '../middlewares/content-type'
 import { bodyParser } from '../middlewares/body-parser'
 import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
