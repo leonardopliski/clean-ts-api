cc5f127d1be90b728dafb70d69d7cfdd4b6523f8
leonardopliski <leonardopliski@gmail.com>
2021-07-06 23:19:11 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,5 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 
 const PORT = 5050
 
-app.listen(5050, () => console.log(`Server running at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
