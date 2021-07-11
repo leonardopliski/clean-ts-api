67fe9e2bed0d1ebd472b2104065e64fdb8cdc483
leonardopliski <leonardopliski@gmail.com>
2021-07-11 02:39:27 -0300

    test: ensure mongodb reconnects if client connection is lost

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -8,7 +8,7 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('account')
+    const accountCollection = await MongoHelper.getCollection('account')
     await accountCollection.deleteMany({})
   })
 
