23141bf1ccc1e1d6508088b72ae16fb2dea7a03d
leonardopliski <leonardopliski@gmail.com>
2021-07-31 13:18:08 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -2,6 +2,7 @@ export interface IAuthenticationModel {
   email: string
   password: string
 }
+
 export interface IAuthentication {
   auth: (authentication: IAuthenticationModel) => Promise<string>
 }
