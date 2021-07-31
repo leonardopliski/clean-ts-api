b6a1ae7d14f9497c4f3475195abf6d4a38ad6ee9
leonardopliski <leonardopliski@gmail.com>
2021-07-31 20:54:59 -0300

    feat: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,7 +1,8 @@
 import { IHasher } from '../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
+import { IHashComparer } from '../../data/protocols/cryptography/hash-comparer'
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   private readonly salt: number
 
   constructor (salt: number) {
@@ -11,5 +12,10 @@ export class BcryptAdapter implements IHasher {
   async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
     return hash
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    await bcrypt.compare(value, hash)
+    return await new Promise(resolve => resolve(true))
   }
 }
