99fd8b2ee021a8faf7b9f997794d30f265f7763c
leonardopliski <leonardopliski@gmail.com>
2021-08-02 23:08:33 -0300

    refactor: rename signup to signup factory

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 import { makeLoginController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
