fb680b69d4206b50ede5e25f5519d160b8bd4fe2
leonardopliski <leonardopliski@gmail.com>
2021-08-02 22:20:40 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -8,7 +8,7 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('account')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
