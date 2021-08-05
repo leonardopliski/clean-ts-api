12823097a7839bf2c729dd91af7c0a31f1bf41f4
leonardopliski <leonardopliski@gmail.com>
2021-08-05 19:53:12 -0300

    chore: change dev script to use docker compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://database-mongo:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://root:example@database-mongo/clean-node-api?authSource=admin',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '^[0sO)H2@8B!W8,'
 }
