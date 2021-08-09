8a885d81288b621391ebdb4f0f2e37e74061b33c
leonardopliski <leonardopliski@gmail.com>
2021-08-09 19:09:06 -0300

    chore: enable debugging with vscode and sourcemaps

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://root:example@database-mongo/clean-node-api?authSource=admin',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://root:example@localhost:27017/clean-node-api?authSource=admin',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '^[0sO)H2@8B!W8,'
 }
