d0e7d72114f0ab08b3e82cf93589222d7777c3a7
leonardopliski <leonardopliski@gmail.com>
2021-08-15 00:34:05 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,43 @@
+import { IAccountModel } from '../../domain/models/account'
+import { ILoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers'
 import { AuthMiddleware } from './auth-middleware'
 
+const makeFakeAccount = (): IAccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password'
+})
+
 describe('Auth Middleware', () => {
   test('should return 403 if no x-access-token exists in headers ', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements ILoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<IAccountModel> {
+        return await new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('should call LoadAccountByToken with correct accessToken ', async () => {
+    class LoadAccountByTokenStub implements ILoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<IAccountModel> {
+        return await new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
