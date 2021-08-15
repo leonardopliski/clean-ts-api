a46ffa173f9a4632b00ac54cc9ad7bb3d0df64a2
leonardopliski <leonardopliski@gmail.com>
2021-08-15 01:33:15 -0300

    refactor: make DbLoadAccountByToken call LoadAccountByTokenRepository with accessToken

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -11,7 +11,7 @@ export class DbLoadAccountByToken implements ILoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<IAccountModel> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
         return account
       }
