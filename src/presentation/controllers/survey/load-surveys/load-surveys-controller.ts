8c796713357721d0652a9def6474decea880742f
leonardopliski <leonardopliski@gmail.com>
2021-08-18 18:42:16 -0300

    feat: ensure LoadSurveysController returns 204 if no surveys are found

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,5 +1,5 @@
 import { ILoadSurveys } from '../../../../domain/usecases/load-surveys'
-import { ok, serverError } from '../../../helpers'
+import { noContent, ok, serverError } from '../../../helpers'
 import { IController, IHttpRequest, IHttpResponse } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements IController {
@@ -8,7 +8,10 @@ export class LoadSurveysController implements IController {
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      if (surveys.length) {
+        return ok(surveys)
+      }
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
