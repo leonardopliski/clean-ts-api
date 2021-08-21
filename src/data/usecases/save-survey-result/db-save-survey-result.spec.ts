7fa644653c4397d695051d79e24bc8ab582a1ea5
leonardopliski <leonardopliski@gmail.com>
2021-08-21 13:40:11 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct data

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,52 @@
+import { TSaveSurveyResultModel, TSurveyResultModel, ISaveSurveyResultRepository } from './db-save-survey-result-protocols'
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import MockDate from 'mockdate'
+
+const makeFakeSurveyResultData = (): TSaveSurveyResultModel => ({
+  surveyId: 'any_survey_id',
+  accountId: 'any_account_id',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeFakeSurveyResult = (): TSurveyResultModel => Object.assign({}, makeFakeSurveyResultData(), {
+  id: 'any_id'
+})
+
+const makeSaveSurveyResultRepository = (): ISaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements ISaveSurveyResultRepository {
+    async save (data: TSaveSurveyResultModel): Promise<TSurveyResultModel> {
+      return await new Promise(resolve => resolve(makeFakeSurveyResult()))
+    }
+  }
+  return new SaveSurveyResultRepositoryStub()
+}
+
+interface TSut {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: ISaveSurveyResultRepository
+}
+
+const makeSut = (): TSut => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+  return { sut, saveSurveyResultRepositoryStub }
+}
+
+describe('SaveSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    const surveyResultData = makeFakeSurveyResultData()
+    await sut.save(surveyResultData)
+    expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
+  })
+})
