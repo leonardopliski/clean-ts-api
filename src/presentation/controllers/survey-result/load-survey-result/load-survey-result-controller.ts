7e5b1d48208c955391e70c2f6d4ada47161e6da2
leonardopliski <leonardopliski@gmail.com>
2021-09-03 20:03:35 -0300

    feat: ensure LoadSurveyResultController returns 403 if no survey is found

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,10 +1,15 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers'
 import { IController, THttpRequest, THttpResponse, ILoadSurveyById } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements IController {
   constructor (private readonly loadSurveyById: ILoadSurveyById) {}
 
   async handle (httpRequest: THttpRequest): Promise<THttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
     return await Promise.resolve(null)
   }
 }
