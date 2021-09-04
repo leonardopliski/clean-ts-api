68d88202d832ce44baea81c111fb460ad3b8b1a9
leonardopliski <leonardopliski@gmail.com>
2021-09-03 21:01:36 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,16 +1,22 @@
+import { ILoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers'
 import { IController, THttpRequest, THttpResponse, ILoadSurveyById } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements IController {
-  constructor (private readonly loadSurveyById: ILoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: ILoadSurveyById,
+    private readonly loadSurveyResult: ILoadSurveyResult
+  ) {}
 
   async handle (httpRequest: THttpRequest): Promise<THttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      const surveyId = httpRequest.params.surveyId
+      const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.loadSurveyResult.load(surveyId)
       return null
     } catch (error) {
       return serverError(error)
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,6 +1,7 @@
 import { ISaveSurveyResult, TSaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { TSurveyResultModel } from '@/domain/models/survey-result'
 import { mockSurveyResultModel } from '@/domain/test'
+import { ILoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 
 export const mockSaveSurveyResult = (): ISaveSurveyResult => {
   class SaveSurveyResultStub implements ISaveSurveyResult {
@@ -9,4 +10,13 @@ export const mockSaveSurveyResult = (): ISaveSurveyResult => {
     }
   }
   return new SaveSurveyResultStub()
+}
+
+export const mockLoadSurveyResult = (): ILoadSurveyResult => {
+  class LoadSurveyResultStub implements ILoadSurveyResult {
+    async load (surveyId: string): Promise<TSurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultStub()
 }
